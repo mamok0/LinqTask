80a2c7719b7e441a2fb6a67461ba1b31215bc0d8
Artem Kivachuk <mamokosf@gmail.com>
2019-11-29 16:50:53 +0300

    task is done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "A BB CC A BB CC CC";
+            string[] words = str.Split(' ');
+            var wordsCount = words.GroupBy(word => word).Select(word => new { Value = word.Key, Count = word.Count() });
+
+            foreach (var word in wordsCount)
+            {
+                Console.WriteLine($"{word.Value}: {word.Count}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
